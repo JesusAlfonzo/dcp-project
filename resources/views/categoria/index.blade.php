d139e7975eab2f088a19e818228ed484ecee3ce8
Frank <frankdtg2004@gmail.com>
2025-02-10 21:26:54 -0400

    migraciones - ep4

---
+++ resources/views/categoria/index.blade.php
@@ -0,0 +1,20 @@
+@extends('template')
+
+@push('css')
+    
+@endpush
+
+@section('title', 'Categorías')
+
+@section('content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Categorías</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">Inicio</li>
+    </ol>
+</div>
+@endsection
+
+@push('js')
+    
+@endpush
